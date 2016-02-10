5e961f6fb98f248a75c727c023bd4188d2848fca
Aman Jain <amannayak15@gmail.com>
2016-02-10 17:06:55 +0530

    Initial Commit

---
+++ RandomFlatColor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomFlatColor
+//
+//  Created by aman jain on 10/02/16.
+//  Copyright © 2016 aman jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
